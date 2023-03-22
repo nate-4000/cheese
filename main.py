c709f183c53acd47e6f93f385f520dfa07c4b8a0
nate-4000 <nathanieltebo8@gmail.com>
2023-03-22 15:47:02 -0700

    !?

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ repre = {
 
 while True:
     x.make_moves_from_current_position([x.get_top_moves()[0]["Move"]])
-    curplayer = !curplayer
+    curplayer = not curplayer
     print("%s's move" % (repre[curplayer]))
     print(x.get_board_visual())
     if x.get_top_moves()[0]["Mate"]:
