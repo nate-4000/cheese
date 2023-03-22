62d00f0f92ecf8a4ade74d98dbc3d2857274a354
nate-4000 <nathanieltebo8@gmail.com>
2023-03-22 15:46:40 -0700

    better looking

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import stockfish
 whereFish = input("wheres my fish? ")
 x = stockfish.Stockfish(whereFish)
 
-while not x.get_top_moves()[0]["Mate"]:
+curplayer = True # white
+
+repre = {
+    True: "White",
+    False: "Black"
+}
+
+while True:
     x.make_moves_from_current_position([x.get_top_moves()[0]["Move"]])
+    curplayer = !curplayer
+    print("%s's move" % (repre[curplayer]))
     print(x.get_board_visual())
+    if x.get_top_moves()[0]["Mate"]:
+        break
+
+print("Mate")
+
